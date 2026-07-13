328cf2010218fa23f59ae7d187a0a25ac281a409
susanaudrey85-eng <susanaudrey85@gmail.com>
2026-07-13 22:46:51 +0300

    feat: add depletion estimate viewset

--- apps/backend/telemetry/views.py
+++ apps/backend/telemetry/views.py
@@ -3,10 +3,10 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import filters, mixins, viewsets
 
 from telemetry.filters import ReadingFilter
-from telemetry.models import Reading
-from telemetry.permissions import ReadingPermission
-from telemetry.selectors import reading_list_for
-from telemetry.serializers import ReadingSerializer
+from telemetry.models import DepletionEstimate, Reading
+from telemetry.permissions import DepletionEstimatePermission, ReadingPermission
+from telemetry.selectors import depletion_estimate_list_for, reading_list_for
+from telemetry.serializers import DepletionEstimateSerializer, ReadingSerializer
 
 
 @extend_schema_view(
@@ -41,3 +41,27 @@ class ReadingViewSet(
 
     def get_queryset(self):
         return reading_list_for(self.request.user, self.request)
+
+
+@extend_schema_view(
+    list=extend_schema(
+        tags=["LPG Analytics - Depletion Estimates"],
+        summary="List depletion estimates",
+    ),
+    retrieve=extend_schema(
+        tags=["LPG Analytics - Depletion Estimates"],
+        summary="Retrieve a depletion estimate",
+    ),
+)
+class DepletionEstimateViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = DepletionEstimate.objects.none()
+    serializer_class = DepletionEstimateSerializer
+    permission_classes = (DepletionEstimatePermission,)
+    ordering = ("-generated_at",)
+
+    def get_queryset(self):
+        return depletion_estimate_list_for(self.request.user)
